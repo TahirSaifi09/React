07cb0f22ccce3937e6e9f2ebea4f7fc7306d9581
Tahir Saifi <tahirsaifi413@gmail.com>
2025-03-09 14:44:57 +0530

    Increase Number with useState

---
+++ useState/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hooks from './page'
+
+function App() {
+
+  return (
+    <div>
+      <Hooks />
+    </div>
+  )
+}
+
+export default App
---
+++ useState/src/page/index.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+// import React, {useState} from "react"; 
+export default function Hooks(){
+
+    const [count, setCount] = useState(0)
+    
+    // function increase(){
+    //     setCount(count + 1)
+    // }
+
+    const increase =() =>{
+        setCount(count + 2)
+    }
+    return(
+        <div className="bg-black h-screen w-screen">
+            <button onClick={increase}>You presed me {count} time</button>
+
+        </div>
+    )
+}
