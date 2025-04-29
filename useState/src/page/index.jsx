b51e9f36ea3cc86c01c7d11b08896ded6935f675
tahirsaifi09 <tahirsaifi413@gmail.com>
2025-04-29 16:54:05 +0530

    Added car details throw useState

--- useState/src/page/index.jsx
+++ useState/src/page/index.jsx
@@ -4,7 +4,12 @@ export default function Hooks(){
 
     const [count, setCount] = useState(0)
     const [color, setColor] = useState("red")
-
+    const [car, setCar] = useState({
+        brand:"Ford",
+        model:"Mustang",
+        year:2025,
+        color:"Green"
+    })
 
     
     // function increase(){
@@ -15,12 +20,16 @@ export default function Hooks(){
         setCount(count + 1)
     }
     return(
-        <div className="bg-black h-screen w-screen ">
+        <div className="bg-black w-screen ">
             <button onClick={increase}>You presed me {count} time</button>
             <h1>This color is {color}</h1>
             <button onClick={()=>setColor("blue")}>Blue</button>
             <button onClick={()=>setColor("Green")}>Green</button>
             <button onClick={()=>setColor("Black")}>black</button>
+            <div>
+                <h1>This is {car.brand} car.</h1>
+                <p>It is a {car.color} {car.model} from {car.year}</p>
+            </div>
         </div>
     )
 }
