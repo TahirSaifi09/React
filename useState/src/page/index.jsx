337fac3ce532c91fc31b28f43050ff327a93e049
tahirsaifi09 <tahirsaifi413@gmail.com>
2025-04-29 16:45:41 +0530

    Added button to can color change

--- useState/src/page/index.jsx
+++ useState/src/page/index.jsx
@@ -3,6 +3,9 @@ import React, { useState } from "react";
 export default function Hooks(){
 
     const [count, setCount] = useState(0)
+    const [color, setColor] = useState("red")
+
+
     
     // function increase(){
     //     setCount(count + 1)
@@ -12,9 +15,12 @@ export default function Hooks(){
         setCount(count + 1)
     }
     return(
-        <div className="bg-black h-screen w-screen">
+        <div className="bg-black h-screen w-screen ">
             <button onClick={increase}>You presed me {count} time</button>
-
+            <h1>This color is {color}</h1>
+            <button onClick={()=>setColor("blue")}>Blue</button>
+            <button onClick={()=>setColor("Green")}>Green</button>
+            <button onClick={()=>setColor("Black")}>black</button>
         </div>
     )
 }
